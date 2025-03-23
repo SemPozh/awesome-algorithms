614870054bd05715de19ae20da9bdbc39d3ebdcc
SemPozh <SemPozh@mail.ru>
2025-03-23 09:23:30 +0300

    changed partition method to Hoare

--- src/main/java/com/algo/nth_element/Main.java
+++ src/main/java/com/algo/nth_element/Main.java
@@ -3,7 +3,7 @@ package com.algo.nth_element;
 public class Main {
     public static void main(String[] args) {
         Integer[] arr = {2, 4, 1, 5, 3, 2, 9, 8, 11};
-        NthElementUtil.nthElement(arr, 3);
+        NthElementUtil.nthElement(arr, 0);
         for (Integer el : arr) {
             System.out.println(el);
         }
--- src/main/java/com/algo/nth_element/NthElementUtil.java
+++ src/main/java/com/algo/nth_element/NthElementUtil.java
@@ -9,12 +9,13 @@ public class NthElementUtil {
         }
 
         int depthLimit = 2 * (int) Math.log(array.length);
-        introSelect(array, 0, array.length - 1, depthLimit);
+        introSelect(array, 0, array.length - 1, n, depthLimit);
     }
 
     private static <T extends Comparable<T>> void introSelect(T[] array,
                                                               int left,
                                                               int right,
+                                                              int n,
                                                               int depthLimit) {
         if (left == right) {
             return;
@@ -26,25 +27,39 @@ public class NthElementUtil {
         }
 
         int pivotIndex = partition(array, left, right);
-        if (array.length < pivotIndex) {
-            introSelect(array, left, pivotIndex - 1, depthLimit - 1); // Ищем в левой части
-        } else if (array.length > pivotIndex) {
-            introSelect(array, pivotIndex + 1, right, depthLimit - 1); // Ищем в правой части
+
+        if (n < pivotIndex) {
+            introSelect(array, left, pivotIndex - 1, n, depthLimit - 1); // Ищем в левой части
+        } else if (n > pivotIndex) {
+            introSelect(array, pivotIndex + 1, right, n, depthLimit - 1); // Ищем в правой части
         }
     }
 
+    // Hoare partitioning
     private static <T extends Comparable<T>> int partition(T[] array, int left, int right) {
-        T pivot = array[right];
-        int i = left;
+        T pivot = array[left];
+        int i = left - 1;
+        int j = right + 1;
 
-        for (int j = left; j < right; j++) {
-            if (array[j].compareTo(pivot) < 0) {
-                swap(array, i, j);
+        while (true) {
+            // Find leftmost element greater
+            // than or equal to pivot
+            do {
                 i++;
+            } while (array[i].compareTo(pivot) < 0);
+
+            // Find rightmost element smaller
+            // than or equal to pivot
+            do {
+                j--;
+            } while (array[j].compareTo(pivot) > 0);
+
+            // If two pointers met.
+            if (i >= j) {
+                return j;
             }
+            swap(array, i, j);
         }
-        swap(array, i, right);
-        return i;
     }
 
     private static <T> void swap(T[] array, int i, int j) {
