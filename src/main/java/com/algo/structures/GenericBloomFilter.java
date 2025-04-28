c7f292edaff826c8d3bfbb76cb603741596ea0e0
blue <blue@itmo.ru>
2025-04-28 16:26:13 +0300

    Generic Bloom Filter realization, cutted

--- src/main/java/com/algo/structures/GenericBloomFilter.java
+++ src/main/java/com/algo/structures/GenericBloomFilter.java
@@ -1,4 +1,22 @@
 package com.algo.structures;
 
 public class GenericBloomFilter<T> {
+    private final BloomFilter bloomFilter;
+
+    public GenericBloomFilter(BloomFilter bloomFilter) {
+        this.bloomFilter = bloomFilter;
+    }
+
+    public void put(T element) {
+        if (element instanceof Integer) {
+            bloomFilter.putInt((Integer) element);
+        }
+    }
+
+    public boolean mightContain(T element) {
+        if (element instanceof Integer) {
+            return bloomFilter.mightContainInt((Integer) element);
+        }
+        return false;
+    }
 }
